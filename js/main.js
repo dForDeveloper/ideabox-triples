8ee7ec53dde243fe0662be6ce39f743e733053dd
Jeo D <e8gea6w5z8id@opayq.com>
2018-10-22 19:11:11 -0600

    Integrate main and idea JS

--- js/main.js
+++ js/main.js
@@ -1,31 +1,46 @@
 get('.save').addEventListener('click', function(event) {
   event.preventDefault();
-  clearInput();
-  let id = localStorage.length;
-  let title = get('#title').value;
-  let body = get('#body').value;
-  let newIdea = new Idea(id, title, body);
+  var id = localStorage.length;
+  // var title = document.getElementById('title-input').value;
+  // var body = document.getElementById('body-input').value;
+  var title = get('#title-input').value;
+  var body = get('#body-input').value;
+  var newIdea = new Idea(id, title, body);
+  newIdea.saveToStorage();
   addCard(newIdea);
-})
+  clearInput();
+});
 
 function get(element) {
   return document.querySelector(element);
 }
 
 function clearInput() {
-  get('#title').value = null;
-  get('#body').value = null;
+  get('#title-input').value = null;
+  get('#body-input').value = null;
 }
 
 function addCard(idea) {
-  let newCard = document.createElement('article');
-  newCard.innerHTML = `<h2>${idea.title}</h2>
+  var newCard = document.createElement('article');
+  newCard.dataset.id = idea.id;
+  newCard.innerHTML =
+  `<h2>${idea.title}</h2>
   <p>${idea.body}</p>
-  <div>
+  <div class="idea-footer">
     <img src="images/downvote.svg" alt="downvote">
     <img src="images/upvote.svg" alt="upvote">
     Quality: ${idea.quality}
     <img src="images/delete.svg" alt="delete">
   </div>`;
-  select('section').prepend(newCard);
+  get('section').prepend(newCard);
 }
+
+/*
+Updating Cards
+  User will click on idea to edit
+  User will press return on idea to commit changes
+  event listener will 
+    get the object by id from .dataset
+    get changes to title and body, getID from dom
+    call updateSelf(id, title, body);
+*/
