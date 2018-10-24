09ebab0085d6ee0fcbde3e3d8636ff8788c4c28e
Justin <justin@baal-software.com>
2018-10-23 20:31:22 -0600

    Refactor according to js style guide

--- js/main.js
+++ js/main.js
@@ -3,14 +3,12 @@ function generateIdeas() {
     var newIdea = new Idea(i, `title ${i}`, `body ${i}`);
     newIdea.saveToStorage();
   }
-}  
+}
 
 var ideas = {};
-get('.save').addEventListener('click', function(event) {
+get('.save').addEventListener('click', function (event) {
   event.preventDefault();
-
   var id = parseInt(localStorage.key(localStorage.length - 1)) + 1;
-
   var title = get('#title-input').value;
   var body = get('#body-input').value;
   var newIdea = new Idea(id, title, body);
@@ -20,14 +18,14 @@ get('.save').addEventListener('click', function(event) {
   clearInput();
 });
 
-get('body').addEventListener('click', function(event) {
+get('body').addEventListener('click', function (event) {
   if (event.target.classList.contains('delete')) {
 
     ideas[event.target.closest('article').dataset.id].deleteFromStorage();
     delete ideas[event.target.closest('article').dataset.id];
     event.target.closest('article').remove();
   }
-  if(event.target.closest('article')){
+  if (event.target.closest('article')) {
     event.target.onblur = event => saveUserEdits(event);
   }
   if (event.target.classList.contains('upvote')) {
@@ -41,43 +39,36 @@ get('body').addEventListener('click', function(event) {
     event.target.nextElementSibling.nextElementSibling.innerText = ideas[id].quality;
   }
 
-  if (event.target.closest('button')){
+  if (event.target.closest('button')) {
     sortCards(event)
   }
- });
+});
 
-
-get('section').addEventListener('keypress', function(event){
- if(event.key === 'Enter'){
-
-  saveUserEdits(event);
-
- }
+get('section').addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    saveUserEdits(event);
+  }
 })
 
-window.onload = function(){
+window.onload = function () {
   var ideaCount = localStorage.length;
   var parsedObj, tempIdea;
 
-  for (var i = 0; i < ideaCount; i++){
+  for (var i = 0; i < ideaCount; i++) {
     parsedObj = JSON.parse(localStorage.getItem(localStorage.key(i)));
     tempIdea = new Idea(parsedObj.id, parsedObj.title, parsedObj.body, parsedObj.quality);
     var tempID = tempIdea.id;
-    ideas[`${tempID}`]  = tempIdea;
+    ideas[`${tempID}`] = tempIdea;
     addCard(tempIdea);
   }
-
-  // document.querySelectorAll('.editable').forEach(e => e.addEventListener('blur', e => {
-  //   saveUserEdits(e)}));
 }
 
-function saveUserEdits(event){
+function saveUserEdits(event) {
   var id = event.target.closest('article').dataset.id
   var cardTitle = get(`article[data-id='${id}'] .card-title`).innerText;
   var cardBody = get(`article[data-id='${id}'] .card-body`).innerText;
   ideas[id].updateSelf(cardTitle, cardBody);
   event.target.blur();
-
 }
 
 function get(element) {
@@ -93,7 +84,7 @@ function addCard(idea) {
   var newCard = document.createElement('article');
   newCard.dataset.id = idea.id;
   newCard.innerHTML =
-  `<h2 class="card-title editable" contenteditable="true">${idea.title}</h2>
+    `<h2 class="card-title editable" contenteditable="true">${idea.title}</h2>
   <p class="card-body editable" contenteditable="true">${idea.body}</p>
   <div class="idea-footer">
   <img src="images/downvote.svg" alt="downvote" class="downvote">
@@ -101,42 +92,17 @@ function addCard(idea) {
   Quality: <span class="quality">${idea.quality}</span>
   <img src="images/delete.svg" alt="delete" class="delete">
   </div>`;
-  // newCard.addEventListener('blur', e => saveUserEdits(e));
   get('section').prepend(newCard);
-
 }
 
-function sortCards(e){
+function sortCards(e) {
   e.preventDefault();
-  document.querySelectorAll('.quality').forEach(function(span){
-   if(span.innerText !== e.target.innerText.toLowerCase() && span.closest('article').classList.value !== 'hidden'){
-    span.closest('article').classList.add('hidden');
-    console.log('hide');
-  } else {
-      
-        span.closest('article').classList.remove('hidden');
-
-      
-      console.log('hsdsdi');
-
-  }
-});
-
+  document.querySelectorAll('.quality').forEach(function (span) {
+    if (span.innerText !== e.target.innerText.toLowerCase() && span.closest('article').classList.value !== 'hidden') {
+      span.closest('article').classList.add('hidden');
+      console.log('hide');
+    } else {
+      span.closest('article').classList.remove('hidden');
+    }
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
